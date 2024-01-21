cad2a01f08d90d3c3f26a17fb591ef61a7ed8c2e
masuodfathi <Masuod.fathi@gmail.com>
2024-01-22 01:36:04 +0330

    practicing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const { indexOf, slice } = require("lodash");
+import { Map } from "immutable";
+
+let book = Map({title:'Harry Potter'});
+
+function Publish(book){
+    return book.set("isPublished",true);
+}
+
+book = Publish(book);
+console.log(book.toJS());
+console.log(book.get("title"));
